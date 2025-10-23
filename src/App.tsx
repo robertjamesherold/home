8a3f4943f30277c0229f7293bf79ddbfc7eab7a4
Robert <robertjamesherold@icloud.com>
2025-10-23 09:11:57 +0200

    icons & badge

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <div className='flex flex-row gap-4'>
       <MetaBadge size={60}/>
-      <SocialIcon icon='GitHub' size={60}/>
-      <SocialIcon icon='LinkedIn' size={60}/>
-      <SocialIcon icon='Mail' size={60}/>
+      <SocialIcon icon='GitHub' color='purple' hue={700} size={60}/>
+      <SocialIcon icon='LinkedIn' color='pink' hue={400} size={60}/>
+      <SocialIcon icon='Mail' color='pink' hue={400} size={60}/>
 
 
     </div>
